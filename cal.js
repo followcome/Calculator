a571634cf1b837e318f407c44b836ca37a792110
Followcome <oyedejisamuel2217@gmail.com>
2023-08-04 15:08:32 +0100

    first commit

---
+++ cal.js
@@ -0,0 +1,53 @@
+function oneBtn(){
+    display.innerHTML+=1
+}
+function twoBtn(){
+    display.innerHTML+=2
+}
+function threeBtn(){
+    display.innerHTML+=3
+}
+function fourBtn(){
+    display.innerHTML+=4
+}
+function fiveBtn(){
+    display.innerHTML+=5
+}
+function sixBtn(){
+    display.innerHTML+=6
+}
+function sevenBtn(){
+    display.innerHTML+=7
+}
+function eightBtn(){
+    display.innerHTML+=8
+}
+function nineBtn(){
+    display.innerHTML+=9
+}
+function zeroBtn(){
+    display.innerHTML+=0
+}
+function minusBtn(){
+    display.innerHTML+="-"
+}
+function plusBtn(){
+    display.innerHTML+="+"
+}
+function divisionBtn(){
+    display.innerHTML+="/"
+}
+function dotBtn(){
+    display.innerHTML+="."
+}
+function clearBtn(){
+    display.innerHTML=""
+    answer.innerHTML=""
+}
+function multiplicationBtn(){
+    display.innerHTML+="*"
+}
+function equalBtn(){
+ answer.innerHTML=eval(display.innerHTML)
+
+}
